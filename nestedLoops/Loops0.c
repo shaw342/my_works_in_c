10edb82f1a1e1fa43fa43c3013655fe834223e8e
shaw342 <shawanbarua70@gmail.com>
2023-04-16 16:21:44 +0200

    Loopsreverse

---
+++ nestedLoops/Loops0.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int array[3] = {1,2,3};
+	int i;
+	int tmp;
+
+	for (i = 0 ;i < 3/2;i++)
+	{
+		tmp = array[i];
+		array[i] = array[3-i-1];
+		array[3-i-1] = tmp;
+	}
+	for (i = 0 ; i < 3; i++)
+	{
+		printf("%d\n",array[i]);
+	}
+	
+	return (0);
+}
